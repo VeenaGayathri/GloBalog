e735297bae6f2ad7826539f70dc743344278c337
VeenaGayathri <91724479+VeenaGayathri@users.noreply.github.com>
2022-08-19 22:28:03 +0530

    Update config.py

--- blog/config.py
+++ blog/config.py
@@ -10,7 +10,7 @@ class Config:
 	MAIL_PORT=587
 	MAIL_USE_TLS=True
 	MAIL_USERNAME=os.environ.get('EMAIL_USER')
-	MAIL_PASSWORD=os.environ.get('EMAIL_PASSWORD')
+	MAIL_PASSWORD=ksjagahzvnwmcyqe
 	FLASKS3_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
 	S3_BUCKET=os.environ.get('AWS_STORAGE_BUCKET_NAME')
 	S3_KEY =os.environ.get('AWS_ACCESS_KEY_ID') 
